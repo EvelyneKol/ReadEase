7778e34033fff568c2ff9dec59e03f6725424d04
EvelyneKol <ekolagki@gmail.com>
2024-03-31 19:10:37 +0300

    ReadEase

---
+++ app/src/main/java/com/example/readease3/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,37 @@
+package com.example.readease3.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.readease3.databinding.FragmentProfileBinding;
+
+public class NotificationsFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
