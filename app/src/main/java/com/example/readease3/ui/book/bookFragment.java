2fd18704f6611555ba80f831b2d10a1d786fb805
Karagiannis-Giorg <Karagiannis302@gmail.com>
2024-04-01 21:47:09 +0300

    Διόρθωση ονόματος bindig σταθεράς

--- app/src/main/java/com/example/readease3/ui/book/bookFragment.java
+++ app/src/main/java/com/example/readease3/ui/book/bookFragment.java
@@ -10,18 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.readease3.databinding.BookBinding;
+import com.example.readease3.databinding.FragmentBookBinding;
+import com.example.readease3.databinding.FragmentBookBinding;
 
 public class bookFragment extends Fragment {
 
-    private BookBinding binding;
+    private FragmentBookBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         bookViewModel bookViewModel =
                 new ViewModelProvider(this).get(bookViewModel.class);
 
-        binding = BookBinding.inflate(inflater, container, false);
+        binding = FragmentBookBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
