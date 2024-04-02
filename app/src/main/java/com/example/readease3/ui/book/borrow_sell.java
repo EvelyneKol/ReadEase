e55f7478313ff94c83a0eb2ecbc84ff887557b19
EvelyneKol <ekolagki@gmail.com>
2024-04-02 19:19:20 +0300

    Borrow_sell screen front-end

--- app/src/main/java/com/example/readease3/ui/book/borrow_sell.java
+++ app/src/main/java/com/example/readease3/ui/book/borrow_sell.java
@@ -1,4 +1,4 @@
-package com.example.readease3;
+package com.example.readease3.ui.book;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.readease3.R;
 
 /**
  * A simple {@link Fragment} subclass.
