5c1c9734e747bbfcb94cee7b49a763871bae3200
Karagiannis-Giorg <Karagiannis302@gmail.com>
2024-04-02 16:29:42 +0300

    Διορθώσεις

--- app/src/main/java/com/example/readease3/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/readease3/ui/home/HomeFragment.java
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.readease3.databinding.FragmentHomeBinding;
+import com.example.readease3.databinding.ClientBinding;
+import com.example.readease3.databinding.ClientBinding;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private ClientBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = ClientBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
