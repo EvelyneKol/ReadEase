440a34fe30866149dac1a92f19d77d2a7365a5db
miltiadiss <miltiadesmantes@gmail.com>
2024-04-07 15:06:53 +0300

    commit

--- app/src/main/java/com/example/readease3/borrow_add.java
+++ app/src/main/java/com/example/readease3/borrow_add.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Borrow_Add extends AppCompatActivity {
+public class borrow_add extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_borrow_add);
+        setContentView(R.layout.borrow_add);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
