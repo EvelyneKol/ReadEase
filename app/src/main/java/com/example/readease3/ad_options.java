5fc10066660468deddb8f25d86f2d26e7141f21c
EvelyneKol <ekolagki@gmail.com>
2024-05-08 18:23:16 +0300

    ad button

--- app/src/main/java/com/example/readease3/ad_options.java
+++ app/src/main/java/com/example/readease3/ad_options.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class upload_ebook extends AppCompatActivity {
+public class ad_options extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.upload_ebook);
+        setContentView(R.layout.activity_ad_options);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/readease3/ui/home/user_home_fragment.java
+++ app/src/main/java/com/example/readease3/ui/home/user_home_fragment.java
@@ -6,11 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import androidx.navigation.Navigation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.readease3.R;
 import com.example.readease3.create_add;
@@ -23,13 +23,16 @@ public class user_home_fragment extends Fragment {
     private UserHomeBinding binding;
 
     private Button buttonEbook;
+    private Button adButton;
 
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.user_home, container, false);
 
         buttonEbook = rootView.findViewById(R.id.button3);
+        adButton = rootView.findViewById(R.id.button1);
 
         buttonEbook.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,9 +42,16 @@ public class user_home_fragment extends Fragment {
             }
         });
 
+        adButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to AdOptionsActivity
+                Navigation.findNavController(v).navigate(R.id.action_userHomeFragment_to_adOptionsActivity);
+            }
+        });
+
         return rootView;
     }
-
 
     @Override
     public void onDestroyView() {
