cf877315dfca04fa2f58312b23eb902f21e77315
mariak0 <maria_kouri@outlook.com>
2024-05-13 15:17:02 +0300

    button for quiz testing

--- app/src/main/java/com/example/readease3/ui/profile/profile_fragment.java
+++ app/src/main/java/com/example/readease3/ui/profile/profile_fragment.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -19,16 +20,26 @@ public class profile_fragment extends Fragment {
 
     private ProfileBinding binding;
 
+    private Button QuizButton;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        profile_ViewModel profile_ViewModel =
-                new ViewModelProvider(this).get(profile_ViewModel.class);
 
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.user_home, container, false);
+        profile_ViewModel profile_ViewModel = new ViewModelProvider(this).get(profile_ViewModel.class);
         binding = ProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         final TextView textView = binding.textNotifications;
         profile_ViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        QuizButton= rootView.findViewById(R.id.button5);
+        QuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to AdOptionsActivity
+                Navigation.findNavController(v).navigate(R.id.action_profile_to_quiz);
+            }
+        });
+
         return root;
     }
 
