578cd89925b7a2e70db80b09a34a9f2fa59021cd
EvelyneKol <ekolagki@gmail.com>
2024-05-14 16:17:16 +0300

    add writer home page files

---
+++ app/src/main/java/com/example/readease3/ui/writer/WriterViewModel.java
@@ -0,0 +1,7 @@
+package com.example.readease3.ui.writer;
+
+import androidx.lifecycle.ViewModel;
+
+public class WriterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/readease3/ui/writer/writer.java
@@ -0,0 +1,47 @@
+package com.example.readease3.ui.writer;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import com.example.readease3.R;
+import com.example.readease3.databinding.UserHomeBinding;
+
+public class writer extends Fragment {
+
+    private WriterViewModel mViewModel;
+    private Button ebookButton;
+
+    public static writer newInstance() {
+        return new writer();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_writer, container, false);
+
+        // Initialize ebookButton
+        ebookButton = rootView.findViewById(R.id.ebookbutton);
+
+        // Set click listener for ebookButton
+        ebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to AdOptionsActivity
+                Navigation.findNavController(v).navigate(R.id.action_writer_home_to_ebook);
+            }
+        });
+
+        return rootView;
+    }
+}
